abf98730a796df84db7dfa7f690c2d597d9d663d
Pattker <patr.kusmierz@gmail.com>
2024-03-06 12:10:32 +0100

    cart in files

---
+++ data.js
@@ -0,0 +1,38 @@
+let orderItemsData = [
+    {
+        id: "MargaritaId",
+        name: "MARGARITA",
+        price: 20,
+        desc: "(sos, ser, oregano)",
+        img: "pizza/pizza-1.jpg"
+    },
+    {
+        id: "FunghiId",
+        name: "FUNGHI",
+        price: 25,
+        desc: "(sos, ser, oregano, pieczarki)",
+        img: "pizza/pizza-2.jpg"
+    },
+    {
+        id: "CapriId",
+        name: "Capri",
+        price: 30,
+        desc: "(sos, ser, oregano, szynka)",
+        img: "pizza/pizza-3.jpg"
+    },
+    {
+        id: "SalamiId",
+        name: "Salami",
+        price: 28,
+        desc: "(sos, ser, oregano, salami)",
+        img: "pizza/pizza-4.jpg"
+    },
+    {
+        id: "WiejskaId",
+        name: "Wiejska",
+        price: 31,
+        desc: "(sos, ser, oregano, salami, kiełbasa)",
+        img: "pizza/pizza-5.jpg"
+    }
+
+];
